d2cebf465c8bd418f2c31352733b927006390b75
Todd Perry <todd.perry@myport.ac.uk>
2017-12-24 17:01:56 +0000

    fixed python indentation, site now renders based on json object

--- js/main.js
+++ js/main.js
@@ -1,47 +1,49 @@
 // globals vars:
 var language = 'en';
 
-name = '';
-loc = '';
-age = '24'; // calculate this later
-prof = '';
-english = '';
-japanese = '';
-food = '';
-curr_comment = '';
-curr = '"huq.io"';
-desc_comment = '';
-desc = '';
+var site_data = {'name': '',
+		 'loc': '',
+		 'age': parseInt((Date.now() - new Date(1993, 07, 25)) / (1000*60*60*24*365.25)).toString(),
+		 'job': '',
+		 'english': '',
+		 'japanese': '',
+		 'likefood': '',
+		 'curr_comment': '',
+		 'curr': '"huq.io"',
+		 'desc_comment': '',
+//		 'desc1': '',
+		 'github': '"https://github.com/djentleman"',
+		 'linkedin': '"https://uk.linkedin.com/in/toddperry93"',
+		 'wantedly': '"https://www.wantedly.com/users/29291558"',
+		 'email': '"todd.perry@myport.ac.uk"',
+		 'hashbang': '#!/usr/bin/python3.6',
+		 'classdef': 'class',
+		 'classname': 'Todd',
+		 'classparen': '(Person):',
+		 'initdef': 'def',
+		 'init': '__init__',
+		 'initparen': '(self):',
+		 'super': 'super',
+		 'superparen': '(Todd, self).__init__():',
+		 'sname': 'self.name =',
+		 'sloc': 'self.location =',
+		 'sage': 'self.age =',
+		 'sjob': 'self.job =',
+		 'slang': 'self.languages =',
+		 'sfood': 'self.favourite_food =',
+		 'scurr': 'self.current =',
+		 'sdesc': 'self.description =',
+		 'gcidef': 'def',
+		 'gci': 'get_contact_info',
+		 'gciparen': '(self):',
+		 'gcireturn': 'return',
+		 'sgithub': '"github"',
+		 'slinkedin': '"slinkedin"',
+		 'swantedly': '"wantedly"',
+		 'semail': '"email"'
+};
 
-//contact ingo
-github = '"https://github.com/djentleman"';
-linkedin = '"https://uk.linkedin.com/in/toddperry93"';
-wantedly = '"https://www.wantedly.com/users/29291558"';
-email = '"todd.perry@myport.ac.uk"';
-
-// misc code bits
-hashbang = '#!/usr/bin/python3.6';
-
-classdef = 'class';
-classname = 'Todd';
-classparen = '(Person):';
-
-init = '__init__';
-initparen = '(self):'
-
-superparen = '(Todd, self).__init__()'
-
-sname = 'self.name =';
-sloc = 'self.location =';
-sage = 'self.age =';
-sjob = 'self.job =';
-slang = 'self.languages =';
-sfood = 'self.favourite_food =';
-scurr = 'self.current =';
-sdesc = 'self.description =';
-
-gci = 'get_contact_info';
-gciparen = '(self):';
+var desc1 = '';
 
 function changeLanguage() {
 	if (language == 'en') {
@@ -63,81 +65,39 @@ function setAttr(attribute, str) {
 
 function setLanguage(language) {
 	if (language == 'en') {
-		name = '"Todd Perry"';
-		loc = '"London, UK"';
-		prof = '"Software Engineer"';
-		english = '"English"';
-		japanese = '"Japanese"';
-		food = '"Ramen"';
-		curr_comment = '# Currently Working At';
-		desc_comment = '# TODO: Write Introduction';
-		desc = '"Hello"';
+		site_data.name = '"Todd Perry"';
+		site_data.loc = '"London, UK"';
+		site_data.job = '"Software Engineer"';
+		site_data.english = '"English"';
+		site_data.japanese = '"Japanese"';
+		site_data.likefood = '"Ramen"';
+		site_data.curr_comment = '# Currently Working At';
+		site_data.desc_comment = '# TODO: Write Introduction';
+		desc1 = '"Hello"';
 	} else if (language = 'ja') {
-		name = '"ペリー・トッド"';
-		loc = '"イギリス、ロンドン"';
-		prof = '"スフトウエア開発者"';
-		english = '"英語"';
-		japanese = '"日本語"';
-		food = '"ラーメン"';
-		curr_comment = '# 現在、この会社に働いています';
-		desc_comment = '# TODO: 自己紹介を書く！';
-		desc = '"初めまして"';
+		site_data.name = '"ペリー・トッド"';
+		site_data.loc = '"イギリス、ロンドン"';
+		site_data.job = '"スフトウエア開発者"';
+		site_data.english = '"英語"';
+		site_data.japanese = '"日本語"';
+		site_data.likefood = '"ラーメン"';
+		site_data.curr_comment = '# 現在、この会社に働いています';
+		site_data.desc_comment = '# TODO: 自己紹介を書く！';
+		desc1 = '"初めまして"';
 	}
 	draw()
 }
 
 function draw() {
+		for (var key in site_data) {
+			//console.log(key)
+			setAttr(key, site_data[key])
+		}
 		// there is probably a niceer way to do this, that involves JSON
-		setAttr('name', name);
-		setAttr('location', loc);
-		setAttr('age', age);
-		setAttr('job', prof);
-		setAttr('english', english);
-		setAttr('japanese',  japanese);
-		setAttr('likefood', food);
-		setAttr('curr_comment', curr_comment);
-		setAttr('curr', curr);
-		setAttr('desc_comment', desc_comment);
 
-		setAttr('github', github);
-		setAttr('linkedin', linkedin);
-		setAttr('wantedly', wantedly);
-		setAttr('email', email);
-
-		// code chunks
-		setAttr('hashbang', hashbang);
-
-		setAttr('classdef', classdef);
-		setAttr('classname', classname);
-		setAttr('classparen', classparen);
-
-		setAttr('initdef', 'def');
-		setAttr('init', init);
-		setAttr('initparen', initparen);
-
-		setAttr('super', 'super');
-		setAttr('superparen', superparen);
-
-		setAttr('sname', sname);
-		setAttr('sloc', sloc);
-		setAttr('sage', sage);
-		setAttr('sjob', sjob);
-		setAttr('slang', slang);
-		setAttr('sfood', sfood);
-		setAttr('scurr', scurr);
-		setAttr('sdesc', sdesc);
-
-		setAttr('gcidef', 'def');
-		setAttr('gci', gci);
-		setAttr('gciparen', gciparen);
-		setAttr('gcireturn', 'return');
-		setAttr('sgithub', '"github"');
-		setAttr('slinkedin', '"linkedin"');
-		setAttr('swantedly', '"wantedly"');
-		setAttr('semail', '"email"');
 
 		// description needs to be handled syncronously
-		setAttr('desc1', desc);
+		setAttr('desc1', desc1);
 }
 
 function type(word, attr) {
